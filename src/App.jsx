1507f99e06f4e74aabb783ac26d20406ca57ce05
Zaheen335 <akhtarzaheen335@gmail.com>
2025-04-02 05:01:28 +0530

    xdisplay removed div5

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ console.log(firstName.length === 0 || lastName.length === 0)
         <input onChange={handleLastNameChange} value={lastName} type="text" name="lastName" required/>
       <button type="submit">Submit</button>
     </form>
-       <div>{text}</div>
+       <span>{text}</span>
     </>
   )
 }
